185fb2c346673e6ceb07ea9f45f433c6cbad18cc
Bukankir18 <garenasyakir@gmail.com>
2021-09-06 18:05:43 +0800

    try edit index

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <html>
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE-edge">
@@ -7,40 +6,26 @@
     <title>Novel Harry Potter</title>
     <link href="style.css" rel="stylesheet">
 </head>
-
 <img src="banner.jpg" width="1520" height="190">
-
 <div class="header">
     <h1>Harry Potter</h1>
-
     <div class="navbar">
-
         <?php include "menu.php"; ?>
-
     </div>
-
     <center>
-
         <?php
         echo "<h1>novel</h1>";
         ?>
         <br>
-
         <table cellspacing="2" cellpadding="2" border="0" width="30%">
             <tr>
                 <th><img src="bookH.jpg" height="350px" width="300px"></th>
-
             </tr>
-
             <tr>
                 <th>
                     <h2> RM 59.50 </h2>
                 </th>
             </tr>
-
-
     </center>
-
     </body>
-
 </html>
--- sinopsis.php
+++ sinopsis.php
@@ -7,20 +7,13 @@
     <title>Novel Harry Potter</title>
     <link href="style.css" rel="stylesheet">
 </head>
-
-<img src="banner2.jpg" width="1520" height="190">
+<img src="banner.jpg" width="1520" height="190">
 <h1>Harry Potter</h1>
-
 <?php include "menu.php"; ?>
-
 <p>
-
     <?php
     $about = "Harry Potter is the most miserable, lonely boy you can imagine. He's shunned by his relatives, the Dursleys, who have raised him since he was an infant. ... Harry also learns that, in the wizarding world, he's a hero. When he was an infant, the evil Lord Voldemort killed his parents and then tried to kill Harry too...";
-
     echo "<p>$about</p>";
     ?>
-
     </body>
-
 </html>
